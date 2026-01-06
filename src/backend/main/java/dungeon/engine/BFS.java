cddbad5bcc7874edba2f6199a09616c2e13ec0db
Paul MUSIAL <paul.musial@telecomnancy.eu>
2026-01-06 14:09:19 +0100

    feat #7: Working BFS and first unit test

---
+++ src/backend/main/java/dungeon/engine/BFS.java
@@ -0,0 +1,39 @@
+package dungeon.engine;
+
+import dungeon.engine.tiles.Treasure;
+
+import java.util.*;
+
+public class BFS {
+
+    private Grid grid;
+
+    public BFS(Grid grid) {
+        this.grid = grid;
+
+    }
+
+    public Coords search(Coords start) {
+        Queue<Node> queue = new LinkedList<Node>();
+        Set<Coords> visited = new HashSet<Coords>();
+        queue.add(new Node(start, null));
+        visited.add(start);
+        while (!queue.isEmpty()) {
+            Node curr = queue.poll();
+            if (grid.getTile(curr.getCoords()) instanceof Treasure) {
+                while (curr.getParent() != null && curr.getParent().getParent() != null) {
+                    curr = curr.getParent();
+                }
+                return curr.getCoords();
+            }
+            for (Coords neighboor: grid.getNeighborsCoords(curr.getCoords())) {
+
+                if (!visited.contains(neighboor)) {
+                    visited.add(neighboor);
+                    queue.add(new Node(neighboor, curr));
+                }
+            }
+        }
+        return null;
+    }
+}
