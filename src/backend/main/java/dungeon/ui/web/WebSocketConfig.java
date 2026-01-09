c0b99083ec21386e860060f283b763740536b5c4
angele <angele.denys@telecomnancy.eu>
2026-01-09 16:20:04 +0100

    fix #65: web java

--- src/backend/main/java/dungeon/ui/web/WebSocketConfig.java
+++ src/backend/main/java/dungeon/ui/web/WebSocketConfig.java
@@ -8,6 +8,10 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /** 
+     * Registers the STOMP endpoints for WebSocket communication.
+     * @param registry
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp-endpoint")
@@ -15,6 +19,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .withSockJS();
     }
 
+    /** 
+     * Configures the message broker for WebSocket communication.
+     * @param registry
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
